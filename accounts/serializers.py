bba1773a0a748f7776d05490fa6b9ad00c598e5a
Samuel Maiko <sammymaiko721@gmail.com>
2024-04-02 17:16:36 +0300

    updating email constraint

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=10, write_only=True, required=True)
     first_name = serializers.CharField(max_length=15, required=True)
     last_name = serializers.CharField(max_length=15, required=False)
-    email = serializers.EmailField(max_length=15, required=True)
+    email = serializers.EmailField(max_length=50, required=True)
 
     def create(self, validated_data):
         user=CustomUser.objects.create_user(**validated_data)
